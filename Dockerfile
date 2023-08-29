72f6fd77555441dfe7a342c90e57897b17735ad8
Adland Lee <novastorm@users.noreply.github.com>
2023-08-29 12:18:51 -0700

    update configs
    * update celery service
    * update database config

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
-FROM python:3.7
+FROM python:3.7-slim
 
 RUN mkdir -p /app
 WORKDIR /app
 
 ADD . /app/
 RUN pip install -r requirements.txt
-
 
--- config/development.py
+++ config/development.py
@@ -1,1 +1,6 @@
-DATA_DIR = "data"
+import os
+
+DATA_DIR = os.getenv("DATA_DIR", "data")
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
+CELERY_TASKS_ALWAYS_EAGER = os.getenv("CELERY_TASKS_ALWAYS_EAGER", False)
